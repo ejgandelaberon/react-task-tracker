56f2d0d42ba1869cf8613db2ff4a72325e8ea2ed
Emmanuel Joseph Beron <emsephron@gmail.com>
2022-06-22 18:37:57 +0800

    feat: fetch from db, get, post, put, delete

--- src/App.js
+++ src/App.js
@@ -1,28 +1,63 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { Route, Routes, Navigate } from "react-router-dom"
 
 function App() {
+  const apiUrl = 'http://task-api.test/api/tasks'
   const [tasks, setTasks] = useState([])
 
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasks = await fetchTasks()
+      setTasks(tasks)
+    }
+
+    getTasks()
+  }, [])
+
+  //fetch Tasks
+  const fetchTasks = async () => {
+    const res = await fetch(apiUrl)
+    const data = await res.json()
+    return data
+  }
+
+  //fetch one task
+  const fetchOneTask = async (id) => {
+    const res = await fetch(`${apiUrl}/${id}`)
+    const data = await res.json()
+    return data
+  }
+
   //add task
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1
-    const newTask = { id, ...task }
-    setTasks([...tasks, newTask])
+  const addTask = async (task) => {
+    const res = await fetch(`${apiUrl}/create`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(task)
+    })
+    const data = await res.json()
+    setTasks([...tasks, data])
   }
 
   // delete task
-  const deleteTask = (objTask) => {
+  const deleteTask = async (objTask) => {
+    await fetch(`${apiUrl}/delete/${objTask.id}`, { method: 'DELETE' })
     const filtered = tasks.filter(task => task.id !== objTask.id)
     setTasks(filtered)
-    console.log(`Deleted task: ${objTask.text}`);
+    console.log(`Deleted task: ${objTask.name}`);
   }
 
   //toggle reminder
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const task = await fetchOneTask(id)
+    await fetch(`${apiUrl}/update/${id}`, {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({reminder: !task.reminder})
+    })
     setTasks(tasks.map(task => task.id === id ? { ...task, reminder: !task.reminder} : task))
   }
 
